671c94e2b137439e5c986368bb68554424c0f54b
NecroBumpist <jpriest8@ymail.com>
2012-08-25 10:08:51 -0500

    Localize some stuff The "ship" stuff could probably be put into a single object rather than having individual variables, but it's fine for now.

--- space.lua
+++ space.lua
@@ -4,26 +4,33 @@ Gamestate.space = state
 
 local Enemy = require("enemy")
 
+local spaceship = love.graphics.newImage("gfx/SpaceShip.png")
+local dirt = love.graphics.newImage("gfx/Dirt.png")
+local grass = love.graphics.newImage("gfx/Grass.png")
+local dirtbottom = love.graphics.newImage("gfx/DirtBottom.png")
+local BKG = love.graphics.newImage("gfx/BKG.png")
+
+local ship_x
+local ship_y
+local speed = 100
+
+local level = {0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,2,2,2,2,2,3,3,3,3,3,4,4,4,4,3,3,3,3,3,2,2,2,2,2,1,1,1,1,1,1,1,1,1,1,5,4,3,2,1,1,1,1,1,1,1,1,1,5,5,5,5,5,5,1,1,1,1,1,1,1,1,1,5,6,5,6,5,6,5,6,5,1,1,1,1,1,1,1,1,2,3,4,3,2,3,2,3,4,5,5,5,5,5,4,3,2,1,0}
+
+local level_width = #level
+local scroll_speed = 50
+local level_x
+
+local Enemies = Enemy.Create(love.graphics.newImage("gfx/Enemy2.png"),300,200)
+
+-- Note: state:enter() is called each time we switch to this state (only from
+-- the main menu at the moment) so it is not neccessary to load image files here
+-- We should only change instance specific things, such as replacing the
+-- ships in the default positions
 function state:enter()
-	spaceship = love.graphics.newImage("gfx/SpaceShip.png")
-	dirt = love.graphics.newImage("gfx/Dirt.png")
-	grass = love.graphics.newImage("gfx/Grass.png")
-	dirtbottom = love.graphics.newImage("gfx/DirtBottom.png")
-	BKG = love.graphics.newImage("gfx/BKG.png")
-	
-	
 	ship_x = 64
 	ship_y = 320
-	speed = 100
-	
-	level = {0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,2,2,2,2,2,3,3,3,3,3,4,4,4,4,3,3,3,3,3,2,2,2,2,2,1,1,1,1,1,1,1,1,1,1,5,4,3,2,1,1,1,1,1,1,1,1,1,5,5,5,5,5,5,1,1,1,1,1,1,1,1,1,5,6,5,6,5,6,5,6,5,1,1,1,1,1,1,1,1,2,3,4,3,2,3,2,3,4,5,5,5,5,5,4,3,2,1,0}
-	
-	
-	Enemies = Enemy.Create(love.graphics.newImage("gfx/Enemy2.png"),300,200)
-	
-	level_width = #level
+
 	level_x = -640
-	scroll_speed = 50
 end
 
 function state:update(dt)
@@ -31,7 +38,7 @@ function state:update(dt)
 	level_x = level_x + scroll_speed * dt
 	
 	if level_x > level_width * 32 then
-	
+
 		level_x = -640
 	
 	end
@@ -50,7 +57,7 @@ function state:update(dt)
 	end
 end
 
-function drawlevel()
+local function drawlevel()
 
 
 	for x=1, level_width do		

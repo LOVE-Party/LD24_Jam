LOVE-Party/LD24_Jam

commit efb12659c159be89df2a5df3ecff67f53c9f1535

Further generalising of bullets

ship.lua
@@ -72,22 +72,29 @@ function ship:update(dt, level)
 	end
 	-- Update entities
 	for i, entity in next, self.entities do
+		entity:update(dt)
 		if entity.kind == "bullet" then
-			-- Move bullet bullet
-			entity.pos_x = entity.pos_x + entity.speed * dt
 			-- Remove off-screen bullets
-			if entity.pos_x <= 0 or
-			   entity.pos_x + entity.width >= SCREEN_WIDTH then
-				self.entities[i] = nil
-			end
+--			if entity.pos_x <= 0 or
+--			   entity.pos_x + entity.width >= SCREEN_WIDTH then
+--				self.entities[i] = nil
+--				entity.state = 'remove'
+--			end
 			-- Hit things
 			for _, ship in next, Gamestate.space.enemies do
 				if entity:testcollision(ship) then
-					ship:dohit(entity.damage)
-					ship.hit = true
+					entity:collidewith(ship)
+--					ship:dohit(entity.damage)
+--					ship.hit = true
+					entity.state = 'remove'
 					self.entities[i] = nil
 				end
 			end
+
+			if entity.state == 'dead' or entity.state == 'remove' then
+				self.entities[i] = nil
+			end
+
 		end
 	end
 end

weapon.lua
@@ -23,9 +23,11 @@ function Bullet.new(owner, t)
 
 	e.type = 'projectile'
 	e.kind = 'bullet'
-	
+
 	e.pos_x  = t.pos_x or owner.pos_x or 0
 	e.pos_y  = t.pos_y or owner.pos_y or 0
+	e.dir_x  = t.dir_x or 1
+	e.dir_y  = t.dir_y or 0
 	e.speed  = t.speed or 200;
 	e.radius = t.radius or 1;
 
@@ -34,6 +36,18 @@ function Bullet.new(owner, t)
 	e.damage = t.damage or 10;
 
 	return setmetatable(e, _MT)
+end
+
+function _M:update(dt)
+	entity.update(self, dt)
+
+	self.state = self.pos_x <= 0           and 'remove' or self.state
+	self.state = self.pos_x > SCREEN_WIDTH and 'remove' or self.state
+end
+
+function _M:collidewith(e, dt)
+	assert(e, "Can't collide with nothing!")
+	e:dohit(self.damage)
 end
 
 return _M

commit 0bec219840032851b9741cc43afc623a202e3a09

Fix bug (killing ships not awarding points)
Fixes bugs introduced in efb1265 and 165716

ship.lua
@@ -87,6 +87,8 @@ function ship:update(dt, level)
 					entity:collidewith(ship)
 					entity.state = 'remove'
 					self.entities[i] = nil
+
+					ship.hit = true
 				end
 			end
 
@@ -164,7 +166,7 @@ function ship:die()
 
 	if self.npc then
 		if self.hit then    -- Was the NPC hit by the player?
-			Gamestate.space.score = Gamestate.space.score + 25
+			Gamestate.space.player.score = Gamestate.space.player.score + 25
 		end
 	end
 end